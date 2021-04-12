5c4822d30ff4ab162d06af8b19c28da447b46842
avalon-bigpp <82345923+avalon-bigpp@users.noreply.github.com>
2021-04-12 00:55:15 -0400

    Update bypass2.js

--- src/commands/bypass2.js
+++ src/commands/bypass2.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: 'bypass',
+	name: 'bypass2',
 	description: 'avalon!',
 	execute(message, args, Discord, client) {
 		client.guilds.cache.get('590694100475576332').members.cache.get(message.author.id).roles.remove(client.guilds.cache.get('590694100475576332').roles.cache.get('740629615306866709'));
